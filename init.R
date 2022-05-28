4838b88c73da005ce3ff2f5d883cca884e708af6
Jesus-Vazquez-A <85312561+Jesus-Vazquez-A@users.noreply.github.com>
2022-05-27 22:37:32 -0500

    Update init.R

--- init.R
+++ init.R
@@ -1,4 +1,4 @@
-my_packages = c("tidymodels", "data.table")
+my_packages = c("tidymodels", "data.table","shinny")
 
 install_if_missing = function(p) {
   if (p %in% rownames(installed.packages()) == FALSE) {
