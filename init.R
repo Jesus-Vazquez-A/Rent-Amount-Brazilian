1095694351303546b499fbb46e07952607e3551d
Amado de Jesús Vázquez Acuñas <85312561+Jesus-Vazquez-A@users.noreply.github.com>
2022-06-26 21:58:16 -0500

    add init

---
+++ init.R
@@ -0,0 +1,7 @@
+my_packages = c("package_name_1", "package_name_2", ...)
+install_if_missing = function(p) {
+if (p %in% rownames(installed.packages()) == FALSE) {
+install.packages(p)
+}
+}
+invisible(sapply(my_packages, install_if_missing))
